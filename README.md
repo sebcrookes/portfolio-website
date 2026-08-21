82f904e327f0771a4232324966d6c14c5346ba8c
Sebastian Crookes <309699783+sebcrookes@users.noreply.github.com>
2026-08-21 17:09:27 +0100

    Initial commit (new repo, last repo had 21 commits)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website
+
+This is my personal portfolio website, served through GitHub Pages at https://sebcrookes.co.uk.
---
+++ script.js
@@ -0,0 +1,76 @@
+/* ======= script.js - this file provides the main functionality to the site. ======= */
+
+/* ======= Navigation bar + menu ======= */
+
+function openHamburgerMenu() {
+    let backdrop = document.querySelector("#nav-menu-backdrop");
+    backdrop.style.visibility = "visible";
+    backdrop.style.opacity = 1;
+    backdrop.style.pointerEvents = "auto";
+
+    let menu = document.querySelector("#nav-menu");
+    menu.style.visibility = "visible";
+    menu.style.opacity = 1;
+    menu.style.pointerEvents = "auto";
+}
+
+function closeHamburgerMenu() {
+    let backdrop = document.querySelector("#nav-menu-backdrop");
+    backdrop.style.opacity = 0;
+    backdrop.style.pointerEvents = "none";
+    setTimeout(function() { backdrop.style.visibility = "hidden"; }, 300);
+
+    let menu = document.querySelector("#nav-menu");
+    menu.style.opacity = 0;
+    menu.style.pointerEvents = "none";
+    setTimeout(function() { menu.style.visibility = "hidden"; }, 300);
+}
+
+function menuScrollTo(sectionID) {
+    closeHamburgerMenu();
+    scrollToSection(sectionID);
+}
+
+function scrollToSection(sectionID) {
+    // If the button gives us "top" then we scroll to the top
+    if (sectionID === "top") {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+        return;
+    }
+
+    // Otherwise, we get the position of the element to scroll to, offset it by
+    // the height of the nav bar (to ensure it is visible), and scroll to it.
+
+    let section = document.querySelector("#" + sectionID);
+    let nav = document.querySelector("#nav-blur");
+
+    let position = section.getBoundingClientRect().top + window.pageYOffset - nav.getBoundingClientRect().height;
+
+    window.scrollTo({
+        top: position,
+        behavior: "smooth"
+    });
+}
+
+/* ======= Image enlargement when clicked ======= */
+
+function enlargeImage(imageID) { // It is the caller's job to pass its ID to this function
+    let img = document.getElementById(imageID);
+
+    let scale = 2.0;
+
+    if (img.width * 2 > window.innerWidth) {
+        scale = window.innerWidth / img.width;
+    }
+
+    img.style.transform = "scale(" + scale + ")";
+    img.style.transition = "transform 0.25s ease";
+
+    setTimeout(function() {
+        img.style.transform = "scale(1.0)";
+        img.style.transition = "transform 0.25s ease";
+    }, 1500);
+}
